c7bef274c769b1ec64a25d48696d20e44fc957f6
DanielKyjas <116916840+DanielKyjas@users.noreply.github.com>
2022-11-13 20:56:54 -0600

    Update 2022_11_13_Matrices.cpp

--- 2022_11_13_Matrices.cpp
+++ 2022_11_13_Matrices.cpp
@@ -10,21 +10,53 @@
  int main()
 {
      srand(time(NULL));
-    int** puntero, Fil, Col;
+    int** puntero = 0, Fil, Col;
+    int matriz1[2][3];
+    int dato;
+
+    int filas1 = (sizeof(matriz1[0]) / sizeof(matriz1[0][0]));
+    std::cout << "Filas: " << filas1 << std::endl;
+    int columnas1 = (sizeof(matriz1[0]) / sizeof(matriz1[0][0]));
+    std::cout << "Columnas: " << columnas1 << std::endl;
+
+    for (int i = 0; i < sizeof(matriz1) / (sizeof(matriz1[0])); i++) {
+        for (int j = 0; j < (sizeof(matriz1[0]) / (sizeof(matriz1[0][0]))); j++) {
+            matriz1[i][j] = rand() % 10;
+        }
+    }
+    for (int i = 0; i < sizeof(matriz1) / (sizeof(matriz1[0])); i++) {
+        for (int j = 0; j < (sizeof(matriz1[0]) / (sizeof(matriz1[0][0]))); j++) {
+            std::cout << matriz1[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
     std::cout << "Dime el numero de filas\n";
     std::cin >> Fil;
     std::cout << "Dame el numero de columnas\n";
     std::cin >> Col;
+    if (Fil > 3 && Col > 3) {
 
-    puntero = new int* [Fil]; 
-    for (int i = 0; i < Fil; i++){
-        puntero[i] = new int[Col];
-    }
-    for (int i = 0; i < Fil; i++) {
-        for (int j = 0; j < Col; j++) {
-            *(*(puntero+i)+j) = rand() % 30;
-            std::cout << puntero[i][j] << std::endl; 
+        puntero = new int* [Fil];
+        for (int i = 0; i < Fil; i++) {
+            puntero[i] = new int[Col];
         }
+        for (int i = 0; i < Fil; i++) {
+            for (int j = 0; j < Col; j++) {
+                *(*(puntero + i) + j) = rand() % 30;
+                std::cout << puntero[i][j] << std::endl;
+            }
 
+        }
+    }
+    else {
+        puntero = new int* [Fil];
+        for (int i = 0; i < Fil; i++) {
+            for (int j = 0; j < Col; j++) {
+                std::cout << "Dame el dato";
+                std::cin >> dato;
+                 *(*(puntero + i) + j) = dato;
+                std::cout << puntero[i][j] << std::endl;
+            }
+        }
     }
 }
